6de11c379fc81277ca83427cf522a46a7bab9daf
alexiariza <alexiariza@yahoo.com>
2026-03-19 16:18:26 +0200

    server.js

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
-const express = require("express");
-const response = await fetch(url, {
-    method: "GET",
-    headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Accept": "text/html,application/xhtml+xml",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Referer": "https://irigatii-smart.infinityfreeapp.com/"
-    }
-});
+import express from "express";
+import pkg from "pg";
+import cors from "cors";
+
+const { Pool } = pkg;
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 app.use(express.json());
 
-app.post("/insert", async (req, res) => {
+const pool = new Pool({
+  connectionString: "postgresql://iot_db_3whd_user:ql42HXPaY8poPqjoIrWz45yjdKSmITmJ@dpg-d6tvtqchg0os738338j0-a.oregon-postgres.render.com/iot_db_3whd",
+  ssl: { rejectUnauthorized: false },
+});
 
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.post("/insert", async (req, res) => {
+  try {
     const { temperature, humidity, pressure, light } = req.body;
 
-    const url = `https://irigatii-smart.infinityfreeapp.com/backend/insert_data.php?temperature=${temperature}&humidity=${humidity}&pressure=${pressure}&light=${light}`;
+    await pool.query(
+      `INSERT INTO sensor_data (temperature, humidity, pressure, illuminance)
+       VALUES ($1, $2, $3, $4)`,
+      [temperature, humidity, pressure, light]
+    );
 
-    try {
-        const response = await fetch(url, {
-            headers: {
-                "User-Agent": "Mozilla/5.0"
-            }
-        });
-
-        const text = await response.text();
-        console.log("PHP RESPONSE:", text);
-
-        res.send("OK");
-
-    } catch (err) {
-        console.log(err);
-        res.send("ERROR");
-    }
+    res.send("OK");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("ERROR");
+  }
 });
 
-app.get("/", (req,res)=>{
-    res.send("API WORKING");
-});
-
-app.listen(10000, () => {
-    console.log("Server running");
-});
+app.listen(process.env.PORT || 10000);
